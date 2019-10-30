1eb968ccd53c968acbc76fe84a40e0fbeecca2c9
tursis <Tursis94@gmail.com>
2019-10-30 16:54:48 +0200

    new changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.conf.urls import include, url
 
+app_name = 'blog'
 urlpatterns = [
- url(r'^$', views.index, name='index')
+ url(r'^$', views.index, name='index'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from  .models import BlogAuthor, Blog
 # Create your views here.
 def index(request):
+
 	return render(request, 'index.html')
+
