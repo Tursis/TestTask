ed5b8ea878c74c02b844cd636a4768dbddf41801
tursis <Tursis94@gmail.com>
2020-01-03 15:45:41 +0200

    create logged_out

--- TestTask/urls.py
+++ TestTask/urls.py
@@ -17,13 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include, url
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('$', include('blog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
-
-
-
--- blog/models.py
+++ blog/models.py
@@ -6,45 +6,46 @@ from django.urls import reverse
 # Create your models here.
 
 class BlogAuthor(models.Model):
-	user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-	bio = models.TextField(max_length=400, help_text="Enter your bio details here.")
-	
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(max_length=400, help_text="Enter your bio details here.")
 
-	def get_absolute_url(self):
-		return reverse('blog:bloggers-detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse('blog:bloggers-detail', args=[str(self.id)])
 
-	def __str__(self):
-		return self.user.username
+    def __str__(self):
+        return self.user.username
 
 
 class Blog(models.Model):
-	name = models.CharField(max_length=200, help_text="Enter your name blog here.")
-	author = models.ForeignKey(BlogAuthor, on_delete=models.SET_NULL, null=True)
-	description = models.TextField(max_length=400, help_text="Enter your blog text here.")
-	pub_date = models.DateTimeField('date published')
+    name = models.CharField(max_length=200, help_text="Enter your name blog here.")
+    author = models.ForeignKey(BlogAuthor, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=400, help_text="Enter your blog text here.")
+    pub_date = models.DateTimeField('date published')
 
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
-	def get_absolute_url(self):
-		return reverse('blog:blog-detail', args=[str(self.id)])
+    def get_absolute_url(self):
+        return reverse('blog:blog-detail', args=[str(self.id)])
+
 
 class BlogComment(models.Model):
-	author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-	description = models.TextField(max_length=400, help_text='Enter your comment here')
-	blog = models.ForeignKey(Blog, on_delete=models.SET_NULL, null=True)
-	pub_date = models.DateTimeField('date published', null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    description = models.TextField(max_length=400, help_text='Enter your comment here')
+    blog = models.ForeignKey(Blog, on_delete=models.SET_NULL, null=True)
+    pub_date = models.DateTimeField('date published', null=True)
 
-	class Meta:
-		ordering = ['pub_date']
+    class Meta:
+        ordering = ['pub_date']
 
-	def __str__(self):
-		"""
-		String for representing the Model object.
-		"""
-		len_title = 75
-		if len(self.description) > len_title:
-			titlestring = self.description[:len_title] + '...'
-		else:
-			titlestring = self.description
-		return titlestring
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        len_title = 75
+        if len(self.description) > len_title:
+            titlestring = self.description[:len_title] + '...'
+        else:
+            titlestring = self.description
+        return titlestring
+
