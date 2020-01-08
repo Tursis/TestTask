00937a80793cbab2ea127798be5a79f35587ad8e
Tursis <tursis94@gmail.com>
2020-01-08 23:58:44 +0200

    reset password

--- TestTask/settings.py
+++ TestTask/settings.py
@@ -120,3 +120,9 @@ USE_TZ = None
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'tursis94@gmail.com'
+EMAIL_HOST_PASSWORD = ''
