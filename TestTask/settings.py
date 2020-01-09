732dcfc5b19161305dd1df8e0a1910102c7f0958
tursis <Tursis94@gmail.com>
2020-01-09 16:48:24 +0200

    last change`s passsrord reset

--- TestTask/settings.py
+++ TestTask/settings.py
@@ -121,8 +121,12 @@ USE_TZ = None
 
 STATIC_URL = '/static/'
 
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tursis94@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST = 'smtp.ukr.net'
+EMAIL_PORT =  465
